a9ca0800e0ca342944e62720ce0a0176abf69f2b
Oleg Grishko <oleggr-brd@yandex.ru>
2024-04-15 21:00:07 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Message from './components/Message';
+import './components/Message.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message mes="Привет всем!" />
+      <Message mes="Как дела?" />
     </div>
   );
 }
---
+++ src/components/Message.js
@@ -0,0 +1,11 @@
+
+
+function Message({ mes }) {
+    return (
+        <div className="Message">
+            <p>Сообщение: {mes}</p>
+        </div>
+    );
+}
+
+export default Message;
